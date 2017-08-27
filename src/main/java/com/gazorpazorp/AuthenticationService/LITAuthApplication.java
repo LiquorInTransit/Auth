b18dedc7a4887b70cb5b4ebdf8dbd8e6e4753a44
Christopher Peckover <christopher.peckover@gmail.com>
2017-08-27 02:09:28 -0400

    SBA Config and Endpoint Config
    -enabled SBA client
    -altered oauth endpoints/servlet contextPath
    	-servelt contextPath no long /uaa. Now all oauth enpoints(custom
    controller and default token issuer) use /uaa on their own
    -customized actuator endpoints (context path and which ones are active)
    -update .gitignore to ignore log.log file
    
    NOTE: Security with SBA is awful with oauth active as well

--- src/main/java/com/gazorpazorp/AuthenticationService/LITAuthApplication.java
+++ src/main/java/com/gazorpazorp/AuthenticationService/LITAuthApplication.java
@@ -2,24 +2,24 @@ package com.gazorpazorp.AuthenticationService;
 
 import java.util.Arrays;
 
-import javax.annotation.PostConstruct;
-
-import org.hsqldb.util.DatabaseManagerSwing;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
-import org.springframework.context.annotation.Profile;
+import org.springframework.core.annotation.Order;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.common.exceptions.OAuth2Exception;
@@ -35,19 +35,21 @@ import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 import org.springframework.security.oauth2.provider.token.store.KeyStoreKeyFactory;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import com.gazorpazorp.service.LITUserDetailsService;
 
 @SpringBootApplication(scanBasePackages="com.gazorpazorp")
 @EnableJpaRepositories("com.gazorpazorp.repository")
 @EntityScan(basePackages="com.gazorpazorp")
+@EnableDiscoveryClient
 public class LITAuthApplication {
 
 	//@Profile("!test")
-	@Configuration
-	@EnableDiscoveryClient
-	public static class EurekaClientConfiguration {		
-	}
+//	@Configuration
+//	@EnableDiscoveryClient
+//	public static class EurekaClientConfiguration {		
+//	}
 	
 //	@PostConstruct
 //	public void getDbManager(){
@@ -149,6 +151,7 @@ public class LITAuthApplication {
 			tokenEnhancerChain.setTokenEnhancers(Arrays.asList(/*customTokenEnhancer, */accessTokenConverter()));
 			
 			endpoints
+			.pathMapping("/oauth/token", "/uaa/oauth/token")
 			.tokenStore(tokenStore())
 			//.accessTokenConverter(accessTokenConverter())
 			.tokenEnhancer(tokenEnhancerChain)
