bc674c38cb02ea930222c9d0aecb63a4ae539d08
Christopher Peckover <christopher.peckover@gmail.com>
2017-09-14 01:24:03 -0400

    Added admin user to dataset

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -7,3 +7,6 @@ insert into USER (id, first_name, last_name, email, username, password, roles, e
 insert into USER (id, first_name, last_name, email, username, password, roles, enabled, non_expired, non_locked) values (3, 'Daniel', 'Dennis', 'daniel@dennis.com', 'dadennis', '$2a$06$8k96vMdJX/nZQa.THDEPnerp0V6gyI4h1V2wXHWZ/998dvwYPX1AK', 'CUSTOMER,DRIVER', TRUE, TRUE, TRUE);
 insert into USER (id, first_name, last_name, email, username, password, roles, enabled, non_expired, non_locked) values (4, 'Nadine', 'Midany', 'nadine@midany.com', 'namidany', '$2a$06$8k96vMdJX/nZQa.THDEPnerp0V6gyI4h1V2wXHWZ/998dvwYPX1AK', 'CUSTOMER', TRUE, TRUE, TRUE);
 insert into USER (id, first_name, last_name, email, username, password, roles, enabled, non_expired, non_locked) values (5, 'John', 'Smith', 'john@smith.com', 'josmith', '$2a$06$8k96vMdJX/nZQa.THDEPnerp0V6gyI4h1V2wXHWZ/998dvwYPX1AK', 'CUSTOMER,DRIVER', TRUE, TRUE, TRUE);
+
+/*ADMIN ACCOUNT*/
+insert into USER (id, first_name, last_name, email, username, password, roles, enabled, non_expired, non_locked) values (6, 'Chris', 'Peckover', 'admin@lit.com', 'admin', '$2a$06$8kFP4x.f.mwxZ7UOplIy4ueeY0m71k8bkvxzYweOZVXCn4z8f94ei', 'ADMIN,ACTUATOR', TRUE, TRUE, TRUE);
