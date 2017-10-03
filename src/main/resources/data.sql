d1e1a3a864ec6717a852344cc4339b43ee0c8c30
Christopher Peckover <christopher.peckover@gmail.com>
2017-10-03 01:57:29 -0400

    Disable init.d log
    -doesn't explicitly disable the log, but instead makes it output to
    /dev/null
    -updated admin user to have email of 'admin'

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -9,4 +9,4 @@ insert into USER (id, first_name, last_name, email, password, roles, enabled, no
 insert into USER (id, first_name, last_name, email, password, roles, enabled, non_expired, non_locked) values (5, 'John', 'Smith', 'john@smith.com', '$2a$06$8k96vMdJX/nZQa.THDEPnerp0V6gyI4h1V2wXHWZ/998dvwYPX1AK', 'CUSTOMER,DRIVER', TRUE, TRUE, TRUE);
 
 /*ADMIN ACCOUNT*/
-insert into USER (id, first_name, last_name, email, password, roles, enabled, non_expired, non_locked) values (6, 'Chris', 'Peckover', 'admin@lit.com', '$2a$06$8kFP4x.f.mwxZ7UOplIy4ueeY0m71k8bkvxzYweOZVXCn4z8f94ei', 'ADMIN,ACTUATOR', TRUE, TRUE, TRUE);
+insert into USER (id, first_name, last_name, email, password, roles, enabled, non_expired, non_locked) values (6, 'Chris', 'Peckover', 'admin', '$2a$06$8kFP4x.f.mwxZ7UOplIy4ueeY0m71k8bkvxzYweOZVXCn4z8f94ei', 'ADMIN,ACTUATOR', TRUE, TRUE, TRUE);
