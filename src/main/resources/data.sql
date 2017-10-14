ad368f54f382c66d7a3270988ebbb6d7e7ae2924
Christopher Peckover <christopher.peckover@gmail.com>
2017-10-14 19:45:48 -0400

    Make Nadine a driver

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -5,7 +5,7 @@
 insert into USER (id, first_name, last_name, email, phone, password, roles, enabled, non_expired, non_locked) values (1, 'Chris', 'Peckover', 'cjpeckover@hotmail.ca', '5195739232', '$2a$06$tRC3rmZvTdfpb0/Mqcp0FuR/v16iO1h1PUuDn8JjJ0rIaQxfRZI.i', 'CUSTOMER,DRIVER,ADMIN', TRUE, TRUE, TRUE);
 insert into USER (id, first_name, last_name, email, phone, password, roles, enabled, non_expired, non_locked) values (2, 'Phil', 'Pedersen', 'phil@pedersen.com', '5195739232', '$2a$06$8k96vMdJX/nZQa.THDEPnerp0V6gyI4h1V2wXHWZ/998dvwYPX1AK', 'CUSTOMER,DRIVER', TRUE, TRUE, TRUE);
 insert into USER (id, first_name, last_name, email, phone, password, roles, enabled, non_expired, non_locked) values (3, 'Daniel', 'Dennis', 'daniel@dennis.com', '5195739232', '$2a$06$8k96vMdJX/nZQa.THDEPnerp0V6gyI4h1V2wXHWZ/998dvwYPX1AK', 'CUSTOMER,DRIVER', TRUE, TRUE, TRUE);
-insert into USER (id, first_name, last_name, email, phone, password, roles, enabled, non_expired, non_locked) values (4, 'Nadine', 'Midany', 'nadine@midany.com', '5195739232', '$2a$06$8k96vMdJX/nZQa.THDEPnerp0V6gyI4h1V2wXHWZ/998dvwYPX1AK', 'CUSTOMER', TRUE, TRUE, TRUE);
+insert into USER (id, first_name, last_name, email, phone, password, roles, enabled, non_expired, non_locked) values (4, 'Nadine', 'Midany', 'nadine@midany.com', '5195739232', '$2a$06$8k96vMdJX/nZQa.THDEPnerp0V6gyI4h1V2wXHWZ/998dvwYPX1AK', 'CUSTOMER,DRIVER', TRUE, TRUE, TRUE);
 insert into USER (id, first_name, last_name, email, phone, password, roles, enabled, non_expired, non_locked) values (5, 'John', 'Smith', 'john@smith.com', '5195739232', '$2a$06$8k96vMdJX/nZQa.THDEPnerp0V6gyI4h1V2wXHWZ/998dvwYPX1AK', 'CUSTOMER,DRIVER', TRUE, TRUE, TRUE);
 
 /*ADMIN ACCOUNT*/
